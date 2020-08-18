4e6cc565ff4856b86bdd000869eab22d02d8f143
Luis Schweigard <luis.schweigard@hs-augsburg.de>
2020-08-18 22:42:07 +0200

    Adding first two topics

---
+++ 01_hello_world/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func hello(name string, language string) string {
+	if len(name) == 0 {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name + "!"
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "french":
+		prefix = frenchHelloPrefix
+	case "spanish":
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(hello("World", ""))
+}
---
+++ 02_integers/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+// Add returns the sum of two integer values a and b
+func Add(a, b int) int {
+	return a + b
+}
